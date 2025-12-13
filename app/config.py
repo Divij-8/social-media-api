3c5b46557e42ed90210c51f2543127c60e30da44
Divij Mazumdar <divij.mazumdar@gmail.com>
2025-12-13 19:46:05 +0530

    better env management

---
+++ app/config.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
+
+
+model_config = SettingsConfigDict(
+    env_file=".env",
+    env_file_encoding="utf-8",
+    case_sensitive=True,
+    extra="ignore",
+    env_file_ignore_missing=True
+)
+
+
+class Settings(BaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    DATABASE_URL: str
+
+    model_config = model_config
+
+
+settings = Settings()
