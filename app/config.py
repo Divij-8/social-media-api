5cb6e4c55d4d689f46d4c21553897c5f9a7035cf
Divij Mazumdar <divij.mazumdar@gmail.com>
2025-12-17 09:16:27 +0530

    add

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic import Field
 
 
 model_config = SettingsConfigDict(
-    env_file=".env",
+    env_file="../.env",
     env_file_encoding="utf-8",
     case_sensitive=True,
     extra="ignore",
