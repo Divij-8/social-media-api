d6c06035e0d661b99201e2064186ed7084f08580
Divij Mazumdar <divij.mazumdar@gmail.com>
2025-12-20 11:57:08 +0530

    improved auth

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,9 +1,16 @@
 from datetime import datetime, timedelta, timezone
 import jwt
+from jwt.exceptions import InvalidTokenError
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from sqlmodel import Session, select
 from config import settings
 from models import User
 from security import verify_password
+from database import get_session
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 def create_access_token(data: dict):
@@ -26,4 +33,33 @@ def authenticate_user(username: str, password: str, session: Session):
         return None
     
     return user
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), session: Session = Depends(get_session)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        user_id: int = payload.get("user_id")
+
+        if user_id is None:
+            raise credentials_exception
+        
+    except InvalidTokenError:
+        raise credentials_exception
+    
+
+    user = session.get(User, user_id)
+
+    if user is None:
+        raise credentials_exception
+    
+    return user
+    
+
+
 
